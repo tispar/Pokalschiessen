56959842c9d27ce031d5c095ae91316ae1224846
Kai Schilling <kai.f.schilling@gmail.com>
2022-08-14 18:18:04 +0200

    added test script for tkinter package

---
+++ GuiTest.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+# Test, ob tkinter korrekt funktioniert
+#tkinter._test()
+
+root = tk.Tk()
+root.title("Programmname")
+root.geometry("800x600")
+root.minsize(width=640, height= 480)
+root.maxsize(width=1280, height= 720)
+
+#root.resizable(width= False, height = False)
+
+label1 = tk.Label(root, text="Hallo Welt")
+label1.pack()
+
+root.mainloop()
